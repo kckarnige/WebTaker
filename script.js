4614ac11e39f947df00bb4b4fcb35a70273cd29a
KiCKinTheBucket <kayos156official@gmail.com>
2026-03-23 22:33:01 -0400

    Switched from JSON file to query params
    Working on post stuff hopefully soon

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-//17985 51359
-fetch("./config.json", {
+var config = (q) => { return new URLSearchParams(location.search).get(q) }
+
+var id = config("id") ?? "51359"
+// Use my freaky ahh ID if nothing is queried
+
+if (config("blurBg") != "false") {
+  document.body.classList.add("fancyBlur")
+}
+
+fetch(`https://walltaker.joi.how/api/links/${id}.json`, {
   method: "GET",
   headers: {
     Accept: "application/json",
   },
 })
   .then((res) => res.json())
-  .then((config) => {
-    fetch(`https://walltaker.joi.how/api/links/${config.id}.json`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => {
+  .then((json) => {
 
-        var background = document.createElement("img")
-        background.setAttribute("src", json.post_thumbnail_url)
-        background.setAttribute("id", "bgbg")
-        document.body.append(background)
+    var background = document.createElement("img")
+    background.setAttribute("src", json.post_thumbnail_url)
+    background.setAttribute("id", "bgbg")
+    document.body.append(background)
 
-        console.log(json.post_url)
-        vidFTs = [".webm", ".mp4"]
-        if (vidFTs.some((ext) => json.post_url.includes(ext))) {
-          var vid = document.createElement("video")
-          vid.setAttribute("loop", "")
-          vid.setAttribute("autoplay", "")
-          vid.setAttribute("src", json.post_url)
-          vid.setAttribute("id", "bg")
-          document.body.append(vid)
-        } else {
-          var img = document.createElement("img")
-          img.setAttribute("src", json.post_url)
-          img.setAttribute("id", "bg")
-          document.body.append(img)
-        }
-      })
+    console.log(json.post_url)
+    vidFTs = [".webm", ".mp4"]
+    if (vidFTs.some((ext) => json.post_url.includes(ext))) {
+      var vid = document.createElement("video")
+      vid.setAttribute("loop", "")
+      vid.setAttribute("autoplay", "")
+
+      if (config("controls") == "true") {
+        vid.setAttribute("controls", "")
+      }
+
+      vid.setAttribute("src", json.post_url)
+      vid.setAttribute("id", "bg")
+      document.body.append(vid)
+    } else {
+      var img = document.createElement("img")
+      img.setAttribute("src", json.post_url)
+      img.setAttribute("id", "bg")
+      document.body.append(img)
+    }
   })
